acabc9897c0d70bbeb7266867b7445dd41b442b1
torres10luis <Luis.angel_15@yahoo.com>
2018-06-07 11:27:50 -0700

    monster-fight update property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
   monsterName = 'superman';
+  monsterPower: 0;
+
+  monsters: any[] = [{
+      monsterName: 'superman',
+      monserPower: null,
+     },
+     { monsterName: 'super',
+      montserPower:  null,
+     }
+    ];
+
+    monsterSelected;
+    ngOnInit() {
+      this.pickMonster();
+    }
+
+    pickMonster() {
+      const index = Math.floor(Math.random() * this.monsters.length);
+      this.monsterSelected = this.monsterPower;
+    }
+generatePower() {
+  this.monsterSelected.monsterPower = Math.floor(Math.random() * 101);
+  console.log(this.monsterSelected.monsterPower);
+}
+
+
 }
